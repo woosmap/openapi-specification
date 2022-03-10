4e59e446e63eb83195c209eb70a3c21f455cea24
gael <gsimon@webgeoservices.com>
2022-03-10 10:56:01 +0100

    chore: Removed Referer when using Private_key for geolocation endpoint

--- dist/snippets/woosmap_http_geolocation_position/woosmap_http_geolocation_position.sh
+++ dist/snippets/woosmap_http_geolocation_position/woosmap_http_geolocation_position.sh
@@ -1,4 +1,3 @@
 # [START woosmap_http_geolocation_position]
-curl -L -X GET 'https://api.woosmap.com/geolocation/position?private_key=YOUR_PRIVATE_API_KEY&ip_address=173.79.254.254' \
--H 'Referer: http://localhost'
+curl -L -X GET 'https://api.woosmap.com/geolocation/position?private_key=YOUR_PRIVATE_API_KEY&ip_address=173.79.254.254'
 # [END woosmap_http_geolocation_position]
