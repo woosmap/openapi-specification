c7a4878ba94ba7a12fe20e0d596202659921d5ee
gaelsimon <gael.simon@woosmap.com>
2023-02-07 15:11:44 +0000

    chore: update dist folder [skip ci]

--- dist/snippets/woosmap_http_address_geocode_collection/woosmap_http_address_geocode_collection.java
+++ dist/snippets/woosmap_http_address_geocode_collection/woosmap_http_address_geocode_collection.java
@@ -4,7 +4,7 @@ OkHttpClient client = new OkHttpClient().newBuilder()
 MediaType mediaType = MediaType.parse("text/plain");
 RequestBody body = RequestBody.create(mediaType, "");
 Request request = new Request.Builder()
-  .url("https://api.woosmap.com/address/geocode/json?address=Place%20de%20la%20Resistance&components=country%3AFR&limit=5&key=YOUR_PUBLIC_API_KEY")
+  .url("https://api.woosmap.com/address/geocode/json?address=Place%20de%20la%20Resistance%20Paris&components=country%3AFR&limit=5&key=YOUR_PUBLIC_API_KEY")
   .method("GET", body)
   .addHeader("Referer", "http://localhost")
   .build();
