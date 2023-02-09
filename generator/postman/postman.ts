9deebe46344962ca8af7915433dc9f6063d27dad
gael <gsimon@webgeoservices.com>
2023-02-09 15:34:53 +0100

    fix: fixed Postman TS error

--- generator/postman/postman.ts
+++ generator/postman/postman.ts
@@ -24,7 +24,7 @@ const main = async (argv: any) => {
     {
       collection: data,
     },
-    { headers: { "X-API-KEY": process.env.POSTMAN_API_KEY } }
+    { headers: { "X-API-KEY": process.env.POSTMAN_API_KEY! } }
   );
 };
 
