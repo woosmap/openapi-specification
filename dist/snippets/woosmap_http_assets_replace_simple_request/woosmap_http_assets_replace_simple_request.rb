968f8e14a6e6d8e8263ee6b7dfa15657d5a0a886
gael <gsimon@webgeoservices.com>
2023-02-24 10:37:43 +0100

    feat: Added Simple Requests for devdoc data management api

---
+++ dist/snippets/woosmap_http_assets_replace_simple_request/woosmap_http_assets_replace_simple_request.rb
@@ -0,0 +1,29 @@
+# [START woosmap_http_assets_replace_simple_request]
+require "uri"
+require "json"
+require "net/http"
+
+url = URI("https://api.woosmap.com/stores?private_key=YOUR_PRIVATE_API_KEY")
+
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+request = Net::HTTP::Post.new(url)
+request["content-type"] = "application/json"
+request.body = JSON.dump({
+  "stores": [
+    {
+      "store_id": "store_123",
+      "name": "My first cool store",
+      "location": {
+        "lat": 43.61,
+        "lng": 3.88
+      }
+    }
+  ]
+})
+
+response = https.request(request)
+puts response.read_body
+
+# [END woosmap_http_assets_replace_simple_request]
---
+++ dist/snippets/woosmap_http_assets_update_simple_request/woosmap_http_assets_update_simple_request.node.js
@@ -0,0 +1,33 @@
+// [START woosmap_http_assets_update_simple_request]
+var axios = require('axios');
+var data = JSON.stringify({
+  "stores": [
+    {
+      "store_id": "store_123",
+      "name": "My amazing store",
+      "location": {
+        "lat": 43.61,
+        "lng": 3.88
+      }
+    }
+  ]
+});
+
+var config = {
+  method: 'put',
+  url: 'https://api.woosmap.com/stores?private_key=YOUR_PRIVATE_API_KEY',
+  headers: { 
+    'content-type': 'application/json'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+// [END woosmap_http_assets_update_simple_request]
