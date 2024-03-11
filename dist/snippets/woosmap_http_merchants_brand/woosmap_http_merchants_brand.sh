72a0d91a5ab5d491e0234cdd821f6347e156efdb
gaelsimon <gael.simon@woosmap.com>
2024-03-11 15:37:28 +0000

    chore: update dist folder [skip ci]

--- dist/snippets/woosmap_http_merchants_brand/woosmap_http_merchants_brand.sh
+++
@@ -1,4 +0,0 @@
-# [START woosmap_http_merchants_brand]
-curl -L -X GET 'https://api.woosmap.com/merchants/brand?dirty_name=APPLE.COM%252FBILL&key=YOUR_PUBLIC_API_KEY' \
--H 'Referer: http://localhost'
-# [END woosmap_http_merchants_brand]
